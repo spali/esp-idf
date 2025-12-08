2cb958a9e33201c3c20c4320a11f1b93d7ca616f
laokaiyao <laokaiyao@espressif.com>
2025-12-08 14:46:21 +0800

    feat(tsens): graduate temperature sensor hal component

--- components/esp_hal_ana_conv/esp32s3/temperature_sensor_periph.c
+++ components/esp_hal_ana_conv/esp32s3/temperature_sensor_periph.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "soc/temperature_sensor_periph.h"
+#include "hal/temperature_sensor_periph.h"
 
 const temperature_sensor_attribute_t temperature_sensor_attributes[TEMPERATURE_SENSOR_ATTR_RANGE_NUM] = {
     /*Offset   reg_val  min  max  error */
