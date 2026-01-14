0f76cddc91f7134fb9b1fe6e8a5a2d873ce2d179
wanckl <wanlei@espressif.com>
2026-01-14 20:09:33 +0800

    fix(driver_spi): added bit trans length check for master driver
    Closes https://github.com/espressif/esp-idf/issues/16049
    
    Closes https://github.com/espressif/esp-idf/issues/17725

--- components/hal/esp32h21/include/hal/spi_ll.h
+++ components/hal/esp32h21/include/hal/spi_ll.h
@@ -40,8 +40,9 @@ extern "C" {
 #define HAL_SPI_SWAP_DATA_TX(data, len) HAL_SWAP32((uint32_t)(data) << (32 - len))
 #define SPI_LL_DMA_MAX_BIT_LEN    (1 << 18)    //reg len: 18 bits
 #define SPI_LL_CPU_MAX_BIT_LEN    (16 * 32)    //Fifo len: 16 words
+#define SPI_LL_TX_MINI_EXTRA_BITS 1            //Minimum length of TX non byte aligned data in bits
+#define SPI_LL_RX_MINI_EXTRA_BITS 1            //Minimum length of RX non byte aligned data in bits
 #define SPI_LL_MOSI_FREE_LEVEL    1            //Default level after bus initialized
-
 /**
  * The data structure holding calculated clock configuration. Since the
  * calculation needs long time, it should be calculated during initialization and
