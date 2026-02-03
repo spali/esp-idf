4efd7489839ba61514ee6c04939d547a04b16120
yangfeng <yangfeng@espressif.com>
2026-02-03 16:20:05 +0800

    feat(bt): Add APIs to control transmit power level
    - API to read ACL real RSSI
    - APIs to read/write tx power of inq/iscan/page/pscan/ACL

--- components/bt/host/bluedroid/common/include/common/bluedroid_user_config.h
+++ components/bt/host/bluedroid/common/include/common/bluedroid_user_config.h
@@ -125,6 +125,13 @@
 #define UC_BT_ENC_KEY_SIZE_CTRL_MODE   0
 #endif
 
+//Enable Classic Bluetooth power control vsc
+#ifdef CONFIG_BT_CLASSIC_ENABLE_POWER_CTRL_VSC
+#define UC_BT_CLASSIC_ENABLE_POWER_CTRL_VSC         CONFIG_BT_CLASSIC_ENABLE_POWER_CTRL_VSC
+#else
+#define UC_BT_CLASSIC_ENABLE_POWER_CTRL_VSC         FALSE
+#endif
+
 //BLE
 #ifdef CONFIG_BT_BLE_ENABLED
 #define UC_BT_BLE_ENABLED                   CONFIG_BT_BLE_ENABLED
