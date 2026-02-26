163e0974b348672d02bcf40abdbfbe026a7d1dfd
harshal.patil <harshal.patil@espressif.com>
2026-02-26 12:22:17 +0530

    change(mbedtls/psa_driver_esp_hmac): Use efuse key block instead of efuse block
    - Maintains compatibility of the older esp_hmac_ APIs and the PSA driver

--- components/esp_tee/subproject/components/tee_sec_storage/tee_sec_storage.c
+++ components/esp_tee/subproject/components/tee_sec_storage/tee_sec_storage.c
@@ -157,7 +157,7 @@ static esp_err_t compute_nvs_keys_with_hmac(esp_efuse_block_t key_blk, nvs_sec_c
     // Create opaque key reference
     esp_hmac_opaque_key_t opaque_key = {
         .use_km_key = false,
-        .efuse_block = (uint8_t)key_blk,
+        .efuse_key_id = hmac_key_id,
     };
 
     // Import the opaque key
