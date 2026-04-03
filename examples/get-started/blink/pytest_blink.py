152b5ccb4c60b44eb173a2f5f8050b7de84aedd5
C.S.M <caosenmiao@espressif.com>
2026-04-03 10:58:36 +0800

    ci(esp32s31): Add ci target test for esp32s31

--- examples/get-started/blink/pytest_blink.py
+++ examples/get-started/blink/pytest_blink.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2025 Espressif Systems (Shanghai) CO LTD
+# SPDX-FileCopyrightText: 2022-2026 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: CC0-1.0
 import logging
 import os
@@ -10,8 +10,9 @@ from pytest_embedded_idf.utils import idf_parametrize
 
 @pytest.mark.generic
 @idf_parametrize('target', ['supported_targets'], indirect=['target'])
+@pytest.mark.temp_skip_ci(targets=['esp32s31'], reason='s31 bringup on this module is not done')
 def test_blink(dut: IdfDut) -> None:
     # check and log bin size
     binary_file = os.path.join(dut.app.binary_path, 'blink.bin')
     bin_size = os.path.getsize(binary_file)
-    logging.info('blink_bin_size : {}KB'.format(bin_size // 1024))
+    logging.info(f'blink_bin_size : {bin_size // 1024}KB')
