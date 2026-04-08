spali/esp-idf

commit 88b62b19fd24c5d775f4b8b306c05a641f5d27eb

refactor(ledc): refactored some LEDC LL functions

components/esp_driver_ledc/src/ledc.c
@@ -118,23 +118,23 @@ static const ledc_clk_src_t s_timer_specific_clks[] = LEDC_LL_TIMER_SPECIFIC_CLO
 static esp_err_t ledc_create_sleep_retention_link_cb(void *arg)
 {
 #if SOC_LEDC_SUPPORT_SLEEP_RETENTION
-    sleep_retention_module_t module = ledc_reg_retention_info.module_id;
+    sleep_retention_module_t module = ledc_reg_retention_info[0].module_id;
 
-    esp_err_t err = sleep_retention_entries_create(ledc_reg_retention_info.common.regdma_entry_array,
-                                                   ledc_reg_retention_info.common.array_size,
+    esp_err_t err = sleep_retention_entries_create(ledc_reg_retention_info[0].common.regdma_entry_array,
+                                                   ledc_reg_retention_info[0].common.array_size,
                                                    REGDMA_LINK_PRI_LEDC, module);
     bool slp_retention_create_failed = (err != ESP_OK);
 
     for (int i = 0; i < SOC_LEDC_TIMER_NUM && !slp_retention_create_failed; i++) {
-        err = sleep_retention_entries_create(ledc_reg_retention_info.timer[i].regdma_entry_array,
-                                             ledc_reg_retention_info.timer[i].array_size,
+        err = sleep_retention_entries_create(ledc_reg_retention_info[0].timer[i].regdma_entry_array,
+                                             ledc_reg_retention_info[0].timer[i].array_size,
                                              REGDMA_LINK_PRI_LEDC, module);
         slp_retention_create_failed |= (err != ESP_OK);
     }
 
     for (int j = 0; j < SOC_LEDC_CHANNEL_NUM && !slp_retention_create_failed; j++) {
-        err = sleep_retention_entries_create(ledc_reg_retention_info.channel[j].regdma_entry_array,
-                                             ledc_reg_retention_info.channel[j].array_size,
+        err = sleep_retention_entries_create(ledc_reg_retention_info[0].channel[j].regdma_entry_array,
+                                             ledc_reg_retention_info[0].channel[j].array_size,
                                              REGDMA_LINK_PRI_LEDC, module);
         slp_retention_create_failed |= (err != ESP_OK);
     }
@@ -176,23 +176,13 @@ static bool ledc_slow_clk_calibrate(void)
     return false;
 }
 
-static esp_err_t ledc_enable_intr_type(ledc_mode_t speed_mode, ledc_channel_t channel, ledc_intr_type_t type)
-{
-    if (type == LEDC_INTR_FADE_END) {
-        ledc_hal_set_fade_end_intr(&(p_ledc_obj[speed_mode]->ledc_hal), speed_mode, channel, true);
-    } else {
-        ledc_hal_set_fade_end_intr(&(p_ledc_obj[speed_mode]->ledc_hal), speed_mode, channel, false);
-    }
-    return ESP_OK;
-}
-
 static void _ledc_fade_hw_acquire(ledc_mode_t mode, ledc_channel_t channel)
 {
     ledc_fade_t *fade = s_ledc_fade_rec[mode][channel];
     if (fade) {
         xSemaphoreTake(fade->ledc_fade_sem, portMAX_DELAY);
         portENTER_CRITICAL(&ledc_spinlock);
-        ledc_enable_intr_type(mode, channel, LEDC_INTR_DISABLE);
+        ledc_ll_enable_interrupt(p_ledc_obj[mode]->ledc_hal.dev, LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(mode, channel), false);
         portEXIT_CRITICAL(&ledc_spinlock);
     }
 }
@@ -433,7 +423,7 @@ static bool ledc_speed_mode_ctx_create(ledc_mode_t speed_mode)
             ledc_new_mode_obj->sleep_mode = LEDC_SLEEP_MODE_INVALID;
 #if CONFIG_PM_POWER_DOWN_PERIPHERAL_IN_LIGHT_SLEEP // for targets that is !SOC_LEDC_SUPPORT_SLEEP_RETENTION, retention module should still be inited to avoid TOP PD
             // Initialize sleep retention module for LEDC
-            sleep_retention_module_t module = ledc_reg_retention_info.module_id;
+            sleep_retention_module_t module = ledc_reg_retention_info[0].module_id;
             sleep_retention_module_init_param_t init_param = {
                 .cbs = {
                     .create = {
@@ -818,7 +808,7 @@ static esp_err_t _ledc_set_pin(int gpio_num, bool out_inv, ledc_mode_t speed_mod
     if (old_gpio_rsv_mask & BIT64(gpio_num)) {
         ESP_LOGW(LEDC_TAG, "GPIO %d is not usable, maybe conflict with others", gpio_num);
     }
-    gpio_matrix_output(gpio_num, ledc_periph_signal[speed_mode].sig_out0_idx + channel, out_inv, false);
+    gpio_matrix_output(gpio_num, ledc_periph_signal[0].speed_mode[speed_mode].sig_out0_idx + channel, out_inv, false);
     portENTER_CRITICAL(&ledc_spinlock);
     p_ledc_obj[speed_mode]->occupied_pin_mask[channel] |= BIT64(gpio_num);
     portEXIT_CRITICAL(&ledc_spinlock);
@@ -966,12 +956,12 @@ esp_err_t ledc_channel_config(const ledc_channel_config_t *ledc_conf)
     }
 #if LEDC_USE_RETENTION_LINK
     if (slp_retention_alloc) {
-        if (sleep_retention_module_allocate(ledc_reg_retention_info.module_id) != ESP_OK) {
+        if (sleep_retention_module_allocate(ledc_reg_retention_info[0].module_id) != ESP_OK) {
             ESP_LOGW(LEDC_TAG, "create retention module failed, power domain can't turn off");
         }
     }
     if (slp_retention_free) {
-        sleep_retention_module_free(ledc_reg_retention_info.module_id);
+        sleep_retention_module_free(ledc_reg_retention_info[0].module_id);
     }
 #endif
 
@@ -1215,12 +1205,13 @@ static void IRAM_ATTR ledc_fade_isr(void *arg)
         if (p_ledc_obj[speed_mode] == NULL) {
             continue;
         }
-        ledc_hal_get_fade_end_intr_status(&(p_ledc_obj[speed_mode]->ledc_hal), speed_mode, &intr_status);
+        intr_status = ledc_ll_get_intr_status(p_ledc_obj[speed_mode]->ledc_hal.dev) & LEDC_LL_DUTY_CHANGE_END_INTR_MASK(speed_mode);
+        intr_status >>= __builtin_ctzll(intr_status);
         while (intr_status) {
             ledc_calc_fade_end_channel(&intr_status, &channel);
 
             // clear interrupt
-            ledc_hal_clear_fade_end_intr_status(&(p_ledc_obj[speed_mode]->ledc_hal), speed_mode, channel);
+            ledc_ll_clear_intr_status(p_ledc_obj[speed_mode]->ledc_hal.dev, LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel));
 
             if (s_ledc_fade_rec[speed_mode][channel] == NULL) {
                 //fade object not initialized yet.
@@ -1454,10 +1445,10 @@ static void _ledc_fade_start(ledc_mode_t speed_mode, ledc_channel_t channel, led
     ledc_fade_t *fade = s_ledc_fade_rec[speed_mode][channel];
     fade->mode = fade_mode;
     // Clear interrupt status of channel
-    ledc_hal_clear_fade_end_intr_status(&(p_ledc_obj[speed_mode]->ledc_hal), speed_mode, channel);
+    ledc_ll_clear_intr_status(p_ledc_obj[speed_mode]->ledc_hal.dev, LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel));
     // Enable interrupt for channel
     portENTER_CRITICAL(&ledc_spinlock);
-    ledc_enable_intr_type(speed_mode, channel, LEDC_INTR_FADE_END);
+    ledc_ll_enable_interrupt(p_ledc_obj[speed_mode]->ledc_hal.dev, LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel), true);
     // Set fade state to HW_FADE state for starting the fade
     assert(fade->fsm == LEDC_FSM_IDLE);
     fade->fsm = LEDC_FSM_HW_FADE;
@@ -1534,7 +1525,7 @@ esp_err_t ledc_fade_stop(ledc_mode_t speed_mode, ledc_channel_t channel)
     }
     // Fade state is either HW_FADE or ISR_CAL (there is a fade in process)
     // Disable ledc channel interrupt first
-    ledc_enable_intr_type(speed_mode, channel, LEDC_INTR_DISABLE);
+    ledc_ll_enable_interrupt(p_ledc_obj[speed_mode]->ledc_hal.dev, LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel), false);
     // Config duty to the duty cycle at this moment
     uint32_t duty_cur = ledc_get_duty(speed_mode, channel);
     ledc_duty_config(speed_mode,
@@ -1580,12 +1571,17 @@ esp_err_t ledc_fade_func_install(int intr_alloc_flags)
     }
     LEDC_CHECK(speed_mode < LEDC_SPEED_MODE_MAX, "LEDC not initialized, call ledc_timer_config first", ESP_ERR_INVALID_STATE);
 
+    uint32_t intr_mask = LEDC_LL_DUTY_CHANGE_END_INTR_MASK(LEDC_LOW_SPEED_MODE);
+#if SOC_LEDC_SUPPORT_HS_MODE
+    intr_mask |= LEDC_LL_DUTY_CHANGE_END_INTR_MASK(LEDC_HIGH_SPEED_MODE);
+#endif
+
     //OR intr_alloc_flags with ESP_INTR_FLAG_IRAM because the fade isr is in IRAM
     return esp_intr_alloc_intrstatus(
-               ETS_LEDC_INTR_SOURCE,
+               ledc_periph_signal[0].irq_id,
                intr_alloc_flags | ESP_INTR_FLAG_IRAM,
-               (uint32_t)ledc_hal_get_fade_end_intr_addr(&(p_ledc_obj[speed_mode]->ledc_hal)),
-               LEDC_LL_FADE_END_INTR_MASK,
+               (uint32_t)ledc_ll_get_intr_status_reg(p_ledc_obj[speed_mode]->ledc_hal.dev),
+               intr_mask,
                ledc_fade_isr,
                NULL,
                &s_ledc_fade_isr_handle

components/esp_driver_ledc/test_apps/ledc/main/test_ledc_sleep.cpp
@@ -86,7 +86,7 @@ TEST_CASE("ledc can output after light sleep (LEDC power domain pd)", "[ledc]")
     test_ledc_sleep_retention(true);
 
     // ledc driver does not have channel release, we will do retention release here to avoid memory leak
-    sleep_retention_module_t module = ledc_reg_retention_info.module_id;
+    sleep_retention_module_t module = ledc_reg_retention_info[0].module_id;
     sleep_retention_module_free(module);
     sleep_retention_module_deinit(module);
 }

components/esp_hal_ledc/esp32/include/hal/ledc_ll.h
@@ -15,7 +15,11 @@
 #include "soc/ledc_reg.h"
 #include "soc/dport_reg.h"
 
+#define LEDC_LL_GET(attr)                  (LEDC_LL_ ## attr)
+
 #define LEDC_LL_GET_HW(group_id)           ((group_id == 0) ? &LEDC : NULL)
+
+#define LEDC_LL_GROUP_NUM                  (1)
 
 #define LEDC_LL_DUTY_NUM_MAX       (LEDC_DUTY_NUM_LSCH0_V)
 #define LEDC_LL_DUTY_CYCLE_MAX     (LEDC_DUTY_CYCLE_LSCH0_V)
@@ -23,8 +27,12 @@
 #define LEDC_LL_HPOINT_VAL_MAX     (LEDC_HPOINT_LSCH0_V)
 #define LEDC_LL_FRACTIONAL_BITS    (8)
 #define LEDC_LL_FRACTIONAL_MAX     ((1 << LEDC_LL_FRACTIONAL_BITS) - 1)
-/// Get the mask of the fade end interrupt status register.
-#define LEDC_LL_FADE_END_INTR_MASK  (0xffffUL << LEDC_DUTY_CHNG_END_HSCH0_INT_ENA_S)
+
+/// Get the mask of the duty change end interrupt status register.
+#define LEDC_LL_DUTY_CHANGE_END_INTR_MASK(speed_mode)  ((speed_mode) == LEDC_LOW_SPEED_MODE ? (0xffUL << LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S) : (0xffUL << LEDC_DUTY_CHNG_END_HSCH0_INT_ENA_S))
+
+#define LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel)  ((speed_mode) == LEDC_LOW_SPEED_MODE ? BIT(LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S + (channel)) : BIT(LEDC_DUTY_CHNG_END_HSCH0_INT_ENA_S + (channel)))
+#define LEDC_LL_EVENT_CHANNEL_MASK(speed_mode, channel)             (LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel))
 
 #define LEDC_LL_GLOBAL_CLOCKS { \
                                 LEDC_SLOW_CLK_APB, \
@@ -542,62 +550,58 @@ static inline void ledc_ll_set_idle_level(ledc_dev_t *hw, ledc_mode_t speed_mode
 }
 
 /**
- * @brief Set fade end interrupt enable
+ * @brief Enable LEDC interrupt for specific event mask
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param channel_num LEDC channel index (0-7), select from ledc_channel_t
- * @param fade_end_intr_en The fade end interrupt enable status
- *
- * @return None
+ * @param mask Interrupt enable mask
+ * @param enable True to enable, False to disable
  */
-static inline void ledc_ll_set_fade_end_intr(ledc_dev_t *hw, ledc_mode_t speed_mode, ledc_channel_t channel_num, bool fade_end_intr_en)
+__attribute__((always_inline))
+static inline void ledc_ll_enable_interrupt(ledc_dev_t *hw, uint32_t mask, bool enable)
 {
-    uint32_t value = hw->int_ena.val;
-    uint32_t int_en_base = (speed_mode == LEDC_LOW_SPEED_MODE) ? LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S : LEDC_DUTY_CHNG_END_HSCH0_INT_ENA_S;
-    hw->int_ena.val = fade_end_intr_en ? (value | BIT(int_en_base + channel_num)) : (value & (~(BIT(int_en_base + channel_num))));
+    if (enable) {
+        hw->int_ena.val |= mask;
+    } else {
+        hw->int_ena.val &= ~mask;
+    }
 }
 
 /**
- * @brief Get fade end interrupt status
+ * @brief Get interrupt status.
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param intr_status The fade end interrupt status
  *
- * @return None
+ * @return Interrupt status.
  */
-static inline void ledc_ll_get_fade_end_intr_status(ledc_dev_t *hw, ledc_mode_t speed_mode, uint32_t *intr_status)
+__attribute__((always_inline))
+static inline uint32_t ledc_ll_get_intr_status(ledc_dev_t *hw)
 {
-    uint32_t value = hw->int_st.val;
-    uint32_t int_en_base = (speed_mode == LEDC_LOW_SPEED_MODE) ? LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S : LEDC_DUTY_CHNG_END_HSCH0_INT_ENA_S;
-    *intr_status = (value >> int_en_base) & 0xff;
+    return hw->int_st.val;
 }
 
 /**
- * @brief Get the address of the fade end interrupt status register.
+ * @brief Get the address of the interrupt status register.
  *
  * @param hw Beginning address of the peripheral registers
- * @return Pointer to the fade end interrupt status register.
+ * @return Pointer to the interrupt status register.
  */
-static inline volatile void* ledc_ll_get_fade_end_intr_addr(ledc_dev_t *hw)
+static inline volatile void* ledc_ll_get_intr_status_reg(ledc_dev_t *hw)
 {
-    return &hw->int_st.val;
+    return (volatile void *)&hw->int_st;
 }
 
 /**
- * @brief Clear fade end interrupt status
+ * @brief Clear interrupt status.
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param channel_num LEDC channel index (0-7), select from ledc_channel_t
+ * @param intr_mask Interrupt status mask to clear
  *
  * @return None
  */
-static inline void ledc_ll_clear_fade_end_intr_status(ledc_dev_t *hw, ledc_mode_t speed_mode, ledc_channel_t channel_num)
+__attribute__((always_inline))
+static inline void ledc_ll_clear_intr_status(ledc_dev_t *hw, uint32_t intr_mask)
 {
-    uint32_t int_en_base = (speed_mode == LEDC_LOW_SPEED_MODE) ? LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S : LEDC_DUTY_CHNG_END_HSCH0_INT_ENA_S;
-    hw->int_clr.val = BIT(int_en_base + channel_num);
+    hw->int_clr.val = intr_mask;
 }
 
 /**

components/esp_hal_ledc/esp32/ledc_periph.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2015-2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2015-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -10,11 +10,12 @@
 /*
  Bunch of constants for every LEDC peripheral: GPIO signals
 */
-const ledc_signal_conn_t ledc_periph_signal[2] = {
+const ledc_signal_conn_t ledc_periph_signal[1] = {
     {
-        .sig_out0_idx = LEDC_HS_SIG_OUT0_IDX,
-    },
-    {
-        .sig_out0_idx = LEDC_LS_SIG_OUT0_IDX,
+        .irq_id = ETS_LEDC_INTR_SOURCE,
+        .speed_mode = {
+            [0] = { .sig_out0_idx = LEDC_HS_SIG_OUT0_IDX },
+            [1] = { .sig_out0_idx = LEDC_LS_SIG_OUT0_IDX },
+        },
     }
 };

components/esp_hal_ledc/esp32c2/include/hal/ledc_ll.h
@@ -16,8 +16,11 @@
 #include "soc/system_struct.h"
 #include "hal/assert.h"
 
+#define LEDC_LL_GET(attr)                  (LEDC_LL_ ## attr)
+
 #define LEDC_LL_GET_HW(group_id)           ((group_id == 0) ? &LEDC : NULL)
 
+#define LEDC_LL_GROUP_NUM                  (1)
 #define LEDC_LL_CHANNEL_SUPPORT_OVF_CNT     1
 
 #define LEDC_LL_DUTY_NUM_MAX       (LEDC_DUTY_NUM_CH0_V)
@@ -27,8 +30,13 @@
 #define LEDC_LL_OVF_CNT_MAX        (LEDC_OVF_NUM_CH0_V + 1)
 #define LEDC_LL_FRACTIONAL_BITS    (8)
 #define LEDC_LL_FRACTIONAL_MAX     ((1 << LEDC_LL_FRACTIONAL_BITS) - 1)
-/// Get the mask of the fade end interrupt status register.
-#define LEDC_LL_FADE_END_INTR_MASK  (0x3fUL << LEDC_DUTY_CHNG_END_CH0_INT_ENA_S)
+
+/// Get the mask of the duty change end interrupt status register.
+#define LEDC_LL_DUTY_CHANGE_END_INTR_MASK(speed_mode)  (0x3fUL << LEDC_DUTY_CHNG_END_CH0_INT_ENA_S)
+
+#define LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel)  BIT(LEDC_DUTY_CHNG_END_CH0_INT_ENA_S + (channel))
+#define LEDC_LL_EVENT_CHANNEL_OVF_CNT(speed_mode, channel)          BIT(LEDC_OVF_CNT_CH0_INT_ENA_S + (channel))
+#define LEDC_LL_EVENT_CHANNEL_MASK(speed_mode, channel)             (LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel) | LEDC_LL_EVENT_CHANNEL_OVF_CNT(speed_mode, channel))
 
 #define LEDC_LL_GLOBAL_CLOCKS { \
                                 LEDC_SLOW_CLK_PLL_DIV, \
@@ -524,62 +532,58 @@ static inline void ledc_ll_set_idle_level(ledc_dev_t *hw, ledc_mode_t speed_mode
 }
 
 /**
- * @brief Set fade end interrupt enable
+ * @brief Enable LEDC interrupt for specific event mask
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param channel_num LEDC channel index (0-7), select from ledc_channel_t
- * @param fade_end_intr_en The fade end interrupt enable status
- *
- * @return None
+ * @param mask Interrupt enable mask
+ * @param enable True to enable, False to disable
  */
-static inline void ledc_ll_set_fade_end_intr(ledc_dev_t *hw, ledc_mode_t speed_mode, ledc_channel_t channel_num, bool fade_end_intr_en)
+__attribute__((always_inline))
+static inline void ledc_ll_enable_interrupt(ledc_dev_t *hw, uint32_t mask, bool enable)
 {
-    uint32_t value = hw->int_ena.val;
-    uint32_t int_en_base = LEDC_DUTY_CHNG_END_CH0_INT_ENA_S;
-    hw->int_ena.val = fade_end_intr_en ? (value | BIT(int_en_base + channel_num)) : (value & (~(BIT(int_en_base + channel_num))));
+    if (enable) {
+        hw->int_ena.val |= mask;
+    } else {
+        hw->int_ena.val &= ~mask;
+    }
 }
 
 /**
- * @brief Get fade end interrupt status
+ * @brief Get interrupt status.
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param intr_status The fade end interrupt status
  *
- * @return None
+ * @return Interrupt status.
  */
-static inline void ledc_ll_get_fade_end_intr_status(ledc_dev_t *hw, ledc_mode_t speed_mode, uint32_t *intr_status)
+__attribute__((always_inline))
+static inline uint32_t ledc_ll_get_intr_status(ledc_dev_t *hw)
 {
-    uint32_t value = hw->int_st.val;
-    uint32_t int_en_base = LEDC_DUTY_CHNG_END_CH0_INT_ENA_S;
-    *intr_status = (value >> int_en_base) & 0xff;
+    return hw->int_st.val;
 }
 
 /**
- * @brief Get the address of the fade end interrupt status register.
+ * @brief Get the address of the interrupt status register.
  *
  * @param hw Beginning address of the peripheral registers
- * @return Pointer to the fade end interrupt status register.
+ * @return Pointer to the interrupt status register.
  */
-static inline volatile void* ledc_ll_get_fade_end_intr_addr(ledc_dev_t *hw)
+static inline volatile void* ledc_ll_get_intr_status_reg(ledc_dev_t *hw)
 {
-    return &hw->int_st.val;
+    return (volatile void *)&hw->int_st;
 }
 
 /**
- * @brief Clear fade end interrupt status
+ * @brief Clear interrupt status.
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param channel_num LEDC channel index (0-7), select from ledc_channel_t
+ * @param intr_mask Interrupt status mask to clear
  *
  * @return None
  */
-static inline void ledc_ll_clear_fade_end_intr_status(ledc_dev_t *hw, ledc_mode_t speed_mode, ledc_channel_t channel_num)
+__attribute__((always_inline))
+static inline void ledc_ll_clear_intr_status(ledc_dev_t *hw, uint32_t intr_mask)
 {
-    uint32_t int_en_base = LEDC_DUTY_CHNG_END_CH0_INT_ENA_S;
-    hw->int_clr.val = BIT(int_en_base + channel_num);
+    hw->int_clr.val = intr_mask;
 }
 
 /**

components/esp_hal_ledc/esp32c2/ledc_periph.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2020-2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2020-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,6 +12,9 @@
 */
 const ledc_signal_conn_t ledc_periph_signal[1] = {
     {
-        .sig_out0_idx = LEDC_LS_SIG_OUT0_IDX,
+        .irq_id = ETS_LEDC_INTR_SOURCE,
+        .speed_mode = {
+            [0] = { .sig_out0_idx = LEDC_LS_SIG_OUT0_IDX },
+        },
     }
 };

components/esp_hal_ledc/esp32c3/include/hal/ledc_ll.h
@@ -16,8 +16,11 @@
 #include "soc/system_struct.h"
 #include "hal/assert.h"
 
+#define LEDC_LL_GET(attr)                  (LEDC_LL_ ## attr)
+
 #define LEDC_LL_GET_HW(group_id)           ((group_id == 0) ? &LEDC : NULL)
 
+#define LEDC_LL_GROUP_NUM                  (1)
 #define LEDC_LL_CHANNEL_SUPPORT_OVF_CNT     1
 
 #define LEDC_LL_DUTY_NUM_MAX       (LEDC_DUTY_NUM_LSCH0_V)
@@ -27,8 +30,13 @@
 #define LEDC_LL_OVF_CNT_MAX        (LEDC_OVF_NUM_LSCH0_V + 1)
 #define LEDC_LL_FRACTIONAL_BITS    (8)
 #define LEDC_LL_FRACTIONAL_MAX     ((1 << LEDC_LL_FRACTIONAL_BITS) - 1)
-/// Get the mask of the fade end interrupt status register.
-#define LEDC_LL_FADE_END_INTR_MASK  (0x3fUL << LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S)
+
+/// Get the mask of the duty change end interrupt status register.
+#define LEDC_LL_DUTY_CHANGE_END_INTR_MASK(speed_mode)  (0x3fUL << LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S)
+
+#define LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel)  BIT(LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S + (channel))
+#define LEDC_LL_EVENT_CHANNEL_OVF_CNT(speed_mode, channel)          BIT(LEDC_OVF_CNT_LSCH0_INT_ENA_S + (channel))
+#define LEDC_LL_EVENT_CHANNEL_MASK(speed_mode, channel)             (LEDC_LL_EVENT_CHANNEL_DUTY_CHANGE_END(speed_mode, channel) | LEDC_LL_EVENT_CHANNEL_OVF_CNT(speed_mode, channel))
 
 #define LEDC_LL_GLOBAL_CLOCKS { \
                                 LEDC_SLOW_CLK_APB, \
@@ -525,62 +533,58 @@ static inline void ledc_ll_set_idle_level(ledc_dev_t *hw, ledc_mode_t speed_mode
 }
 
 /**
- * @brief Set fade end interrupt enable
+ * @brief Enable LEDC interrupt for specific event mask
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param channel_num LEDC channel index (0-7), select from ledc_channel_t
- * @param fade_end_intr_en The fade end interrupt enable status
- *
- * @return None
+ * @param mask Interrupt enable mask
+ * @param enable True to enable, False to disable
  */
-static inline void ledc_ll_set_fade_end_intr(ledc_dev_t *hw, ledc_mode_t speed_mode, ledc_channel_t channel_num, bool fade_end_intr_en)
+__attribute__((always_inline))
+static inline void ledc_ll_enable_interrupt(ledc_dev_t *hw, uint32_t mask, bool enable)
 {
-    uint32_t value = hw->int_ena.val;
-    uint32_t int_en_base = LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S;
-    hw->int_ena.val = fade_end_intr_en ? (value | BIT(int_en_base + channel_num)) : (value & (~(BIT(int_en_base + channel_num))));
+    if (enable) {
+        hw->int_ena.val |= mask;
+    } else {
+        hw->int_ena.val &= ~mask;
+    }
 }
 
 /**
- * @brief Get fade end interrupt status
+ * @brief Get interrupt status.
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param intr_status The fade end interrupt status
  *
- * @return None
+ * @return Interrupt status.
  */
-static inline void ledc_ll_get_fade_end_intr_status(ledc_dev_t *hw, ledc_mode_t speed_mode, uint32_t *intr_status)
+__attribute__((always_inline))
+static inline uint32_t ledc_ll_get_intr_status(ledc_dev_t *hw)
 {
-    uint32_t value = hw->int_st.val;
-    uint32_t int_en_base = LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S;
-    *intr_status = (value >> int_en_base) & 0xff;
+    return hw->int_st.val;
 }
 
 /**
- * @brief Get the address of the fade end interrupt status register.
+ * @brief Get the address of the interrupt status register.
  *
  * @param hw Beginning address of the peripheral registers
- * @return Pointer to the fade end interrupt status register.
+ * @return Pointer to the interrupt status register.
  */
-static inline volatile void* ledc_ll_get_fade_end_intr_addr(ledc_dev_t *hw)
+static inline volatile void* ledc_ll_get_intr_status_reg(ledc_dev_t *hw)
 {
-    return &hw->int_st.val;
+    return (volatile void *)&hw->int_st;
 }
 
 /**
- * @brief Clear fade end interrupt status
+ * @brief Clear interrupt status.
  *
  * @param hw Beginning address of the peripheral registers
- * @param speed_mode LEDC speed_mode, high-speed mode or low-speed mode
- * @param channel_num LEDC channel index (0-7), select from ledc_channel_t
+ * @param intr_mask Interrupt status mask to clear
  *
  * @return None
  */
-static inline void ledc_ll_clear_fade_end_intr_status(ledc_dev_t *hw, ledc_mode_t speed_mode, ledc_channel_t channel_num)
+__attribute__((always_inline))
+static inline void ledc_ll_clear_intr_status(ledc_dev_t *hw, uint32_t intr_mask)
 {
-    uint32_t int_en_base = LEDC_DUTY_CHNG_END_LSCH0_INT_ENA_S;
-    hw->int_clr.val = BIT(int_en_base + channel_num);
+    hw->int_clr.val = intr_mask;
 }
 
 /**

components/esp_hal_ledc/esp32c3/ledc_periph.c
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2020-2025 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText: 2020-2026 Espressif Systems (Shanghai) CO LTD
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -12,6 +12,9 @@
 */
 const ledc_signal_conn_t ledc_periph_signal[1] = {
     {
-        .sig_out0_idx = LEDC_LS_SIG_OUT0_IDX,
+        .irq_id = ETS_LEDC_INTR_SOURCE,
+        .speed_mode = {
+            [0] = { .sig_out0_idx = LEDC_LS_SIG_OUT0_IDX },
+        },
     }
 };

commit 9f50fb8543362a37006958e6682a2b2d10d6cb41

fix(ledc): wrong interrupt status handling in ISR could cause INT WDT
Introduced in 88b62b19fd24c5d775f4b8b306c05a641f5d27eb

components/esp_driver_ledc/src/ledc.c
@@ -1206,7 +1206,7 @@ static void IRAM_ATTR ledc_fade_isr(void *arg)
             continue;
         }
         intr_status = ledc_ll_get_intr_status(p_ledc_obj[speed_mode]->ledc_hal.dev) & LEDC_LL_DUTY_CHANGE_END_INTR_MASK(speed_mode);
-        intr_status >>= __builtin_ctzll(intr_status);
+        intr_status >>= __builtin_ctzll(LEDC_LL_DUTY_CHANGE_END_INTR_MASK(speed_mode));
         while (intr_status) {
             ledc_calc_fade_end_channel(&intr_status, &channel);
 

examples/peripherals/ledc/ledc_fade/pytest_ledc_fade.py
@@ -1,0 +1,20 @@
+# SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: CC0-1.0
+import pytest
+from pytest_embedded_idf import IdfDut
+from pytest_embedded_idf.utils import idf_parametrize
+from pytest_embedded_idf.utils import soc_filtered_targets
+
+
+@pytest.mark.generic
+@idf_parametrize('target', soc_filtered_targets('SOC_LEDC_SUPPORTED == 1'), indirect=['target'])
+def test_ledc_fade_example(dut: IdfDut) -> None:
+    # repeat to test at least two rounds of fade
+    dut.expect_exact('1. LEDC fade up to duty = 4000')
+    dut.expect_exact('2. LEDC fade down to duty = 0')
+    dut.expect_exact('3. LEDC set duty = 4000 without fade')
+    dut.expect_exact('4. LEDC set duty = 0 without fade')
+    dut.expect_exact('1. LEDC fade up to duty = 4000')
+    dut.expect_exact('2. LEDC fade down to duty = 0')
+    dut.expect_exact('3. LEDC set duty = 4000 without fade')
+    dut.expect_exact('4. LEDC set duty = 0 without fade')